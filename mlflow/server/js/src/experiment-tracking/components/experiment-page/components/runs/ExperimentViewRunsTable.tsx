fc1f0dd473627aee6a298a39e392ba927a36e17c
Emilio Graff <1@emil.io>
2026-05-27 10:02:58 -0700

    Fix plot view
    Signed-off-by: Emilio Graff <1@emil.io>
    (cherry picked from commit 1a7b05dfd5ce111f1d8f574fbd11a312c927a980)

--- mlflow/server/js/src/experiment-tracking/components/experiment-page/components/runs/ExperimentViewRunsTable.tsx
+++ mlflow/server/js/src/experiment-tracking/components/experiment-page/components/runs/ExperimentViewRunsTable.tsx
@@ -536,7 +536,7 @@ export const ExperimentViewRunsTable = React.memo(
                 onColumnResized={handleColumnResized}
                 onSelectionChanged={onSelectionChange}
                 getRowHeight={rowHeightGetterFn}
-                headerHeight={EXPERIMENT_RUNS_TABLE_ROW_HEIGHT * 2}
+                headerHeight={isComparingRuns ? EXPERIMENT_RUNS_TABLE_ROW_HEIGHT : EXPERIMENT_RUNS_TABLE_ROW_HEIGHT * 2}
                 groupHeaderHeight={EXPERIMENT_RUNS_TABLE_ROW_HEIGHT}
                 onRowSelected={handleRowSelected}
                 suppressRowClickSelection
